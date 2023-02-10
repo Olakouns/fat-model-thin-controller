f6ddf481fa22c6cce7a98958770ac45777cd46fd
olakouns <kounassolazare@gmail.com>
2023-02-10 20:01:11 +0000

    FAT_MODEL

--- src/controllers/lutteurs-controller.js
+++ src/controllers/lutteurs-controller.js
@@ -1,91 +1,33 @@
 const Lutter = require("../models/lutteurs");
 
 exports.getLutters = async (req, res) => {
-    const result = await Lutter.find();
-    res.send(result)
-    // res.render('lutters/index', { title: "Hey olakouns", lutters: result })
+    res.send(await Lutter.getAllLutters())
 }
 
 exports.createLutterGet = (req, res) => {
-    // res.send("createLutterGet");
-    // res.redirect("/lutters");
-    res.render('lutters/create', { title: "Create lutters", lutter: null })
+    // res.render('lutters/create', { title: "Create lutters", lutter: null })
 }
 
-exports.createLutterPost = (req, res) => {
-    let lutter = new Lutter(req.body);
-    lutter.save(err => {
-        console.log(err);
-        if (err != null) {
-            return res.send(err);
-        } else {
-            res.redirect("/lutters");
-        }
-    })
-    // res.send("createLutterPost");
+exports.createLutterPost = async (req, res) => {
+    res.send(await Lutter.addLutter(req.body));
 }
 
-exports.getDetailLutter = (req, res) => {
-    Lutter.findById(
-        req.params.id,
-        (err, value)=> {
-            if (err) return res.send(err);
-            res.send(value)
-            // res.render('lutters/details', { lutterDetails: value })
-        }
-    )
+exports.getDetailLutter = async (req, res) => {
+   res.send(await Lutter.getOneLutter(req.params.id));
 }
-
 
 exports.updateLutterGet = (req, res) => {
-    // res.send("updateLutterGet");
-    Lutter.findById(
-        req.params.id,
-        (err, value)=> {
-            if (err) return res.send(err);
-            res.render('lutters/create', { title: "Update Lutter", lutter: value })
-        }
-    )
+
 }
 
-exports.updateLutterPost = (req, res) => {
-    // res.send("updateLutterPost");
-    Lutter.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new : true},
-        (err, updatedLutter) => {
-            if (err) return res.send(err);
-            res.send(updatedLutter)
-            // res.redirect("/lutters");
-        }
-    )
+exports.updateLutterPost = async (req, res) => {
+    res.send(await Lutter.updateLutter(req.params.id, req.body))
 }
 
 exports.deleteLutterGet = (req, res) => {
-    // Lutter.findByIdAndRemove(
-    //     req.params.id,
-    //     (err, deletedLutter) =>{
-    //         if (err) return res.send(err);
-    //         res.send({
-    //             success : true,
-    //             message : "Lutter is deleted"
-    //         })
-    //     }
-    // )
     res.send("deleteLutterGet");
 }
 
-exports.deleteLutterPost = (req, res) => {
-    Lutter.findByIdAndRemove(
-        req.params.id,
-        (err, deletedLutter) =>{
-            if (err) return res.send(err);
-            res.send({
-                success : true,
-                message : "Lutter is deleted"
-            })
-        }
-    )
-    // res.send("deleteLutterPost");
+exports.deleteLutterPost = async (req, res) => {
+    res.send(await Lutter.deleteLutter(req.params.id));
 }
--- src/models/lutteurs.js
+++ src/models/lutteurs.js
@@ -10,4 +10,41 @@ const LutterSchema = new mongoose.Schema({
 
 const Lutter = mongoose.model('Lutter', LutterSchema)
 
-module.exports =  Lutter 
+
+exports.getAllLutters = async () => {
+    return await Lutter.find();
+}
+
+exports.addLutter = async (newLutter) => {
+    let lutter = new Lutter(newLutter);
+    lutter.save(err => {
+        if (err != null) {
+            result = {
+                success: false,
+                message : `Some error occure :${err} `
+            };
+        } 
+        result =  {
+            success: true,
+            message: "Lutter saved"
+        };
+    })
+}
+
+exports.getOneLutter  = async (idLutter) => {
+    return await Lutter.findById(idLutter)
+}
+
+exports.updateLutter = async (idLutter, lutterUpdate) => {
+    return Lutter.findByIdAndUpdate(
+        idLutter,
+        lutterUpdate,
+        {new : true}
+    )
+}
+
+exports.deleteLutter = async (idLutter) => {
+    return Lutter.findByIdAndRemove(idLutter)
+}
+
+// module.exports =  Lutter 
